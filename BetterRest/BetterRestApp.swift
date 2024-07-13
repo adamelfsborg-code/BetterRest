c611e6694014a9a38d75bd6eb8640ebf091d718a
Adam Elfsborg <adamelfsborg.dev@gmail.com>
2024-07-13 10:02:46 +0200

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Adam Elfsborg on 2024-07-13.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
